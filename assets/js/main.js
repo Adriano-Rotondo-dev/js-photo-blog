45c6dc66eb0167690a5142e4b8ddb051ff55a0a1
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-17 12:19:36 +0200

    Add initial JavaScript structure and TODOs for API integration

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+//TODO: seleziono i DOM nodes necessari
+
+
+
+//TODO: creo la variabile per salvare l'endpoint dell'API https://lanciweb.github.io/demo/api/pictures/
+
+
+
+//TODO: inizializzo l'AJAX request all'endpoint e mi assicuro che funzioni
+
+
+
+//TODO: creo un ciclo forEach per ciclare al suo interno e perndere il singolo elemento
+
+
+
+//TODO: destrutturo l'obj restituito dall'AJAX request per ottenere dateEl e imgEl variables
+
+
+
+//TODO: creo il MarkUp da usare per la singola col e card
+
+
+
+//TODO: rimpiazzo i placeholder con dateEl e imgEl variables
+
+
+
+//TODO: aggiorno il DOM element con il MarkUp
+
+
