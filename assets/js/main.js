028a532c2bfd8ac1f7d73340703ee3cfa9870567
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-17 12:51:51 +0200

    Fix HTML structure and add JavaScript file for dynamic content loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,36 @@
 //TODO: seleziono i DOM nodes necessari
 
+const rowEl = document.getElementById("posts");
 
+//* creo la variabile per salvare l'endpoint dell'API https://lanciweb.github.io/demo/api/pictures/
 
-//TODO: creo la variabile per salvare l'endpoint dell'API https://lanciweb.github.io/demo/api/pictures/
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 
+//* inizializzo l'AJAX request all'endpoint e mi assicuro che funzioni
 
-
-//TODO: inizializzo l'AJAX request all'endpoint e mi assicuro che funzioni
-
-
-
-//TODO: creo un ciclo forEach per ciclare al suo interno e perndere il singolo elemento
-
-
-
-//TODO: destrutturo l'obj restituito dall'AJAX request per ottenere dateEl e imgEl variables
-
-
-
-//TODO: creo il MarkUp da usare per la singola col e card
-
-
-
-//TODO: rimpiazzo i placeholder con dateEl e imgEl variables
-
-
-
-//TODO: aggiorno il DOM element con il MarkUp
-
-
+fetch(endpoint)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    //* creo un ciclo forEach per ciclare al suo interno e prendo il singolo elemento
+    data.forEach((post) => {
+      //*destrutturo l'obj restituito dall'AJAX request per ottenere dateEl e imgEl variables
+      const { date, url } = post;
+      //TODO: creo il MarkUp da usare per la singola col e card
+      const postMarkUp = `<div class="col">
+                            <div class="card">
+                                <img id="pin" src="../assets/img/pin.svg" alt="pin_img">
+                                <div class="card-header">
+                                //* rimpiazzo i placeholder con dateEl e imgEl variables
+                                    <img id="post_img" class="placeholder" src="${url}">
+                                </div>
+                                <div class="cardbody"> ${date} </div>
+                            </div>
+                        </div>`;
+      //* aggiorno il DOM element con il MarkUp
+      rowEl.insertAdjacentHTML("beforeend", postMarkUp);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
